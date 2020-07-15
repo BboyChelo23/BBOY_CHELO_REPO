680b7a047dc3d378a95e28bcb75c09712192e065
BboyChelo23 <68344612+BboyChelo23@users.noreply.github.com>
2020-07-15 14:05:54 -0400

    Proyecto birthday

---
+++ src/utils/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAX7EprTz2_mixghNoXZhnurBI8GvtHxS4',
+  authDomain: 'birthday-2f806.firebaseapp.com',
+  databaseURL: 'https://birthday-2f806.firebaseio.com',
+  projectId: 'birthday-2f806',
+  storageBucket: 'birthday-2f806.appspot.com',
+  messagingSenderId: '835265854922',
+  appId: '1:835265854922:web:16335fc473fb7b4a5a6333',
+};
+
+export default firebase.initializeApp(firebaseConfig);
